722cb3b5b48b4432850292f4c84e415febf552c7
Charles Ellison <crelliso@gmail.com>
2020-08-08 19:28:26 -0500

    Constants
    Added constants and index files

---
+++ server/constants/grant-type.constants.ts
@@ -0,0 +1,5 @@
+export const client_credentials = "client_credentials";
+
+export const authorization_code = "authorization_code";
+
+export const refresh_token = "refresh_token";
---
+++ server/constants/spotify-url.constants.ts
@@ -0,0 +1,6 @@
+// base request url
+export const baseUrl = 'https://api.spotify.com/v1/';
+
+// auth base request url
+export const authorizeUrl = 'https://accounts.spotify.com/authorize';
+export const tokenUrl = 'https://accounts.spotify.com/api/token';
